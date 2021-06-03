18dcadd83e1c29e6695a2f7f35e12f225fc4f248
gokulkumar <gokulkumar2908@gmail.com>
2021-06-03 14:10:03 +0530

    Stack and Que algorith

---
+++ Algorithm/stack.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+class stack:
+    def __init__(self) -> None:
+        self.__arr = deque([])
+
+    def put(self, element):
+        self.__arr.append(element)
+        return
+
+    def get(self):
+        return self.__arr.pop()
+
+
+cls = stack()
+cls.put(5)
+cls.put(54)
+cls.put(52)
+cls.put(56)
+cls.put(58)
+cls.put(59)
+
+print(cls.get())
+print(cls.get())
+print(cls.get())
+print(cls.get())
+print(cls.get())
+print(cls.get())
